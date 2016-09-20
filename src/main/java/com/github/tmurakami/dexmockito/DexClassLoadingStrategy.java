bb807b3ee9521f92dcc85bf253dae9da7df468e3
tmurakami <tmurakami@users.noreply.github.com>
2016-09-20 18:47:52 +0900

    Remove redundant buffering

--- src/main/java/com/github/tmurakami/dexmockito/DexClassLoadingStrategy.java
+++ src/main/java/com/github/tmurakami/dexmockito/DexClassLoadingStrategy.java
@@ -5,7 +5,6 @@ import com.android.dx.cf.direct.StdAttributeFactory;
 import com.android.dx.dex.DexOptions;
 import com.android.dx.dex.cf.CfOptions;
 import com.android.dx.dex.cf.CfTranslator;
-import com.android.dx.dex.file.DexFile;
 
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
@@ -13,7 +12,6 @@ import net.bytebuddy.utility.RandomString;
 
 import org.mockito.internal.util.io.IOUtil;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -22,6 +20,8 @@ import java.util.Map;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
 import java.util.logging.Logger;
+
+import dalvik.system.DexFile;
 
 final class DexClassLoadingStrategy implements ClassLoadingStrategy {
 
@@ -39,46 +39,47 @@ final class DexClassLoadingStrategy implements ClassLoadingStrategy {
                                                Map<TypeDescription, byte[]> types) {
         DexOptions dexOptions = new DexOptions();
         CfOptions cfOptions = new CfOptions();
-        DexFile dexFile = new DexFile(dexOptions);
+        com.android.dx.dex.file.DexFile file = new com.android.dx.dex.file.DexFile(dexOptions);
         for (Map.Entry<TypeDescription, byte[]> entry : types.entrySet()) {
             String path = entry.getKey().getName().replace('.', '/') + ".class";
             byte[] bytes = entry.getValue();
             DirectClassFile cf = new DirectClassFile(bytes, path, false);
             cf.setAttributeFactory(StdAttributeFactory.THE_ONE);
-            dexFile.add(CfTranslator.translate(cf, bytes, cfOptions, dexOptions, dexFile));
+            file.add(CfTranslator.translate(cf, bytes, cfOptions, dexOptions, file));
         }
         String name = randomString.nextString();
         File jar = new File(cacheDir, name + ".jar");
-        JarOutputStream out = null;
-        dalvik.system.DexFile loadedFile = null;
         try {
-            out = new JarOutputStream(new BufferedOutputStream(new FileOutputStream(jar)));
-            out.putNextEntry(new JarEntry("classes.dex"));
-            dexFile.writeTo(out, null, false);
-            out.close();
-            loadedFile = dexFileLoader.loadDex(jar, new File(cacheDir, name + ".dex"), 0);
+            JarOutputStream out = new JarOutputStream(new FileOutputStream(jar));
+            try {
+                out.putNextEntry(new JarEntry("classes.dex"));
+                file.writeTo(out, null, false);
+            } finally {
+                IOUtil.closeQuietly(out);
+            }
             Map<TypeDescription, Class<?>> classMap = new HashMap<>();
-            for (TypeDescription td : types.keySet()) {
-                classMap.put(td, loadClass(loadedFile, td, classLoader));
+            DexFile dexFile = dexFileLoader.loadDex(jar, new File(cacheDir, name + ".dex"), 0);
+            try {
+                for (TypeDescription td : types.keySet()) {
+                    classMap.put(td, loadClass(dexFile, td, classLoader));
+                }
+            } finally {
+                try {
+                    dexFile.close();
+                } catch (IOException ignored) {
+                }
             }
             return classMap;
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         } finally {
-            if (loadedFile != null) {
-                try {
-                    loadedFile.close();
-                } catch (IOException ignored) {
-                }
-            }
-            IOUtil.closeQuietly(out);
             if (jar.exists() && !jar.delete()) {
                 Logger.getLogger("com.github.tmurakami.dexmockito").warning("Cannot delete " + jar);
             }
         }
     }
 
-    private static Class<?> loadClass(dalvik.system.DexFile dexFile,
+    private static Class<?> loadClass(DexFile dexFile,
                                       TypeDescription typeDescription,
                                       ClassLoader classLoader) throws ClassNotFoundException {
         Class<?> c = dexFile.loadClass(typeDescription.getName(), classLoader);
